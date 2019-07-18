6d25c28052b20eb3745aacb2be1e4fa42bba21e7
Aashay Pawar <41710824+aapawar26@users.noreply.github.com>
2019-07-18 18:06:37 +0530

    Add files via upload

---
+++ user/signup.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>
+		XYZ
+	</title>
+</head>
+<body>
+	Name<input type="text" name="Name"><br>
+	City<input type="text" name="City"><br>
+	Phone<input type="text" name="Phone"><br>
+	Email<input type="text" name="Email"><br>
+	Username<input type="text" name="Username"><br>
+	Password<input type="Password" name="Password"><br>
+	<button type="submit" name="Submit"> Submit </button>
+</body>
+</html>
